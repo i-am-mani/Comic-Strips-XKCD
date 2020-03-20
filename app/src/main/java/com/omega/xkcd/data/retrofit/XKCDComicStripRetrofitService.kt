64358aff516cc3d6158a8a97d449f221d4619622
Mandeep <mandeepch868@gmail.com>
2020-03-20 20:14:39 +0530

    fix missing '}'

--- app/src/main/java/com/omega/xkcd/data/retrofit/XKCDComicStripRetrofitService.kt
+++ app/src/main/java/com/omega/xkcd/data/retrofit/XKCDComicStripRetrofitService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Path
 
 interface XKCDComicStripRetrofitService{
 
-    @GET("{comicNumber/info.0.json")
+    @GET("{comicNumber}/info.0.json")
     suspend fun getXKCDComic(@Path("comicNumber") number: Int): XKCDComicStripModel
 
     @GET("info.0.json")
