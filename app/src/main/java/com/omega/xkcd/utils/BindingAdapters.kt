464f98e37633ed6a633b6e108f5867703416b8f5
Mandeep <mandeepch868@gmail.com>
2020-03-21 12:39:49 +0530

    Add progressbar placeholder on glide

--- app/src/main/java/com/omega/xkcd/utils/BindingAdapters.kt
+++ app/src/main/java/com/omega/xkcd/utils/BindingAdapters.kt
@@ -3,14 +3,22 @@ package com.omega.xkcd.utils
 import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 
 
+private val TAG = "BindingAdapter"
+
 @BindingAdapter("app:showImage")
 fun showImageFromUrl(view: ImageView, url: String?) {
-    val TAG = "BindingAdapter"
-    if(url != null) {
-        Glide.with(view).load(url).into(view)
+
+    if (url != null) {
+        val circularProgressDrawable =
+            CircularProgressDrawable(view.context)
+        circularProgressDrawable.strokeWidth = 10f
+        circularProgressDrawable.centerRadius = 60f
+        circularProgressDrawable.start()
+        Glide.with(view).load(url).placeholder(circularProgressDrawable).into(view)
         Log.d(TAG, "binding image view with image from glide")
     }
     Log.d(TAG, "url = $url")
