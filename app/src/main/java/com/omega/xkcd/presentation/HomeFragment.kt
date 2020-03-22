f4d77ef8fb28d4ed9cff489716f606a4df7f23bf
Mandeep <mandeepch868@gmail.com>
2020-03-22 15:36:46 +0530

    manually create Activity Scoped ViewModel

--- app/src/main/java/com/omega/xkcd/presentation/HomeFragment.kt
+++ app/src/main/java/com/omega/xkcd/presentation/HomeFragment.kt
@@ -1,21 +1,34 @@
 package com.omega.xkcd.presentation
 
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.omega.xkcd.databinding.FragmentHomeBinding
+import com.omega.xkcd.domain.repository.ComicStripsRepository
 import com.omega.xkcd.presentation.viewmodels.HomeViewModel
 import com.omega.xkcd.utils.InjectionFragment
+import com.omega.xkcd.utils.KotlinViewModelProvider
 import org.kodein.di.generic.instance
 
 
 class HomeFragment : InjectionFragment() {
 
-    private val mViewModel: HomeViewModel by instance()
+    private lateinit var mViewModel: HomeViewModel
+    private val repository: ComicStripsRepository by instance()
     private val TAG = "HomeFragment"
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val mainActivity = context as MainActivity
+        mViewModel = KotlinViewModelProvider.of(mainActivity){
+            HomeViewModel(repository,context.application)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
