4993bf4a8ce26e60218bb5b29a5a911f65afc251
Mandeep <mandeepch868@gmail.com>
2020-03-25 10:55:07 +0530

    add format date method

--- app/src/main/java/com/omega/xkcd/utils/Utils.kt
+++ app/src/main/java/com/omega/xkcd/utils/Utils.kt
@@ -1,13 +1,23 @@
 package com.omega.xkcd.utils
 
+import android.app.Application
+import android.content.Context
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.widget.TextView
+import android.widget.Toast
+import com.omega.xkcd.ComicStripsApplication
+import com.omega.xkcd.R
 import java.text.SimpleDateFormat
 import java.util.*
 
 object Utils {
 
     @JvmStatic
-    fun getFormatedDate(date: Date):String{
+    fun getFormatedDate(date: Date?):String{
+        if(date != null){
         val dateFormatter = SimpleDateFormat("EEE, d MMM YYYY")
-        return dateFormatter.format(date)
+        return dateFormatter.format(date)}
+        return ""
     }
 }
