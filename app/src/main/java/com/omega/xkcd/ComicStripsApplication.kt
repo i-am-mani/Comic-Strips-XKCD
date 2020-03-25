c2d2ea1439328348e57fbc7aaa17613f2adfdd87
Mandeep <mandeepch868@gmail.com>
2020-03-25 10:55:41 +0530

    optimize imports and remove unused code

--- app/src/main/java/com/omega/xkcd/ComicStripsApplication.kt
+++ app/src/main/java/com/omega/xkcd/ComicStripsApplication.kt
@@ -1,24 +1,18 @@
 package com.omega.xkcd
 
 import android.app.Application
-import android.content.Context
-import android.util.Log
 import com.omega.xkcd.data.dataModule
 import com.omega.xkcd.data.room.ComicStripsDatabase
-import com.omega.xkcd.domain.domainModule
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
-import org.kodein.di.android.x.androidXModule
-import org.kodein.di.generic.*
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+import org.kodein.di.generic.with
 
-/*
-False positive "Unused symbol" for a custom Android application class referenced in AndroidManifest.xml file:
-https://youtrack.jetbrains.net/issue/KT-27971
-*/
 class ComicStripsApplication : Application(), KodeinAware {
 
     override val kodein = Kodein.lazy {
-//        import(androidXModule(this@ComicStripsApplication))
+        //        import(androidXModule(this@ComicStripsApplication))
         bind<ComicStripsDatabase>() with singleton {
             ComicStripsDatabase.getDatabase(this@ComicStripsApplication)
         }
